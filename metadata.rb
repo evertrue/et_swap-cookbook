08d055cf18560c89b4f8232b90b9a9adda1f5ac7
Jeff Byrnes <jeff@evertrue.com>
2014-12-08 11:44:00 -0500

    Add supports statement to metadata to clarify tested usage
    Good to do for open-source cookbooks

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'Apache v2.0'
 description      'Installs/Configures swap_device'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.2'
+
+supports 'ubuntu', '>= 12.04'
