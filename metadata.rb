0d222ad7f82f6e7cc5dafbff44d74bdb05eab412
Jeff Byrnes <jeff@evertrue.com>
2014-12-08 11:43:10 -0500

    Switch to actual open-source license.
    Oops.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'swap_device'
 maintainer       'EverTrue, Inc.'
 maintainer_email 'eric.herot@evertrue.com'
-license          'All rights reserved'
+license          'Apache v2.0'
 description      'Installs/Configures swap_device'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.0.2'
